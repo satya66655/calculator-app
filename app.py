dac16bfb04906a5bea7e9daf9663d5ed8c88551c
Satish <satish@example.com>
2025-05-13 09:55:15 +0000

    Initial commit - Calculator v1 (Addition)

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def calculator():
+    result = None
+    if request.method == 'POST':
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        result = num1 + num2  # only addition for now
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
