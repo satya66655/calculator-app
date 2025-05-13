4dfd56691abf366bd4e9bbaca4e1b0b754a9eeb5
Satish <satish@example.com>
2025-05-13 11:27:51 +0000

    new

--- app.py
+++ app.py
@@ -7,11 +7,9 @@ def calculator():
     result = None
     if request.method == 'POST':
         try:
-            num1 = float(request.form['num1'])
-            num2 = float(request.form['num2'])
-            operator = request.form['operator']
-            print(f"DEBUG: num1={num1}, num2={num2}, operator={operator}")
-            
+            num1 = float(request.form.get('num1', 0))
+            num2 = float(request.form.get('num2', 0))
+            operator = request.form.get('operator')
             if operator == '+':
                 result = num1 + num2
             elif operator == '-':
@@ -20,10 +18,8 @@ def calculator():
                 result = "Unsupported operation"
         except Exception as e:
             result = f"Error: {e}"
-            print(result)
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    print("Starting Flask App...")
     app.run(host='0.0.0.0', port=80)
 
